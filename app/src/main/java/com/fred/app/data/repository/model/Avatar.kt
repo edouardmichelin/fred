791e742d4326a4d6e60983afa5445dbe1dd7e147
edouardmichelin <edouard.michelin15@gmail.com>
2024-12-01 00:53:07 +0100

    feat: add profile avatars

---
+++ app/src/main/java/com/fred/app/data/repository/model/Avatar.kt
@@ -0,0 +1,14 @@
+package com.fred.app.data.repository.model
+
+enum class Avatar(val id: String) {
+    Avocado("avatar_avocado.png"),
+    Book("avatar_book.png"),
+    Campfire("avatar_campfire.png"),
+    Deer("avatar_deer.png"),
+    Cat("avatar_cat.png"),
+    Dog("avatar_dog.png"),
+    Globe("avatar_globe.png"),
+    Leaf("avatar_leaf.png"),
+    Stream("avatar_stream.png"),
+    Tree("avatar_tree.png"),
+}
