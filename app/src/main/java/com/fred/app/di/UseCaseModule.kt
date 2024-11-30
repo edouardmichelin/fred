6a125068628404f9f683c96356255abdf40b223c
edouardmichelin <edouard.michelin15@gmail.com>
2024-11-30 23:21:09 +0100

    chore(di): configure DI for 'creation' use-cases

--- app/src/main/java/com/fred/app/di/UseCaseModule.kt
+++ app/src/main/java/com/fred/app/di/UseCaseModule.kt
@@ -7,6 +7,9 @@ import com.fred.app.data.repository.base.LoginRepository
 import com.fred.app.data.repository.base.RegisterRepository
 import com.fred.app.data.repository.base.VehicleRepository
 import com.fred.app.domain.sdk.AuthService
+import com.fred.app.domain.usecase.CreateActivityUseCase
+import com.fred.app.domain.usecase.CreateLocationUseCase
+import com.fred.app.domain.usecase.CreateVehicleUseCase
 import com.fred.app.domain.usecase.GetActivitiesUseCase
 import com.fred.app.domain.usecase.GetLocationsUseCase
 import com.fred.app.domain.usecase.GetUserUseCase
@@ -63,5 +66,26 @@ class UseCaseModule {
         authService: AuthService,
         vehicleRepository: VehicleRepository,
     ) = GetVehiclesUseCase(authService, vehicleRepository)
+
+    @ViewModelScoped
+    @Provides
+    fun provideCreateActivityUseCase(
+        authService: AuthService,
+        activityRepository: ActivityRepository,
+    ) = CreateActivityUseCase(authService, activityRepository)
+
+    @ViewModelScoped
+    @Provides
+    fun provideCreateLocationUseCase(
+        authService: AuthService,
+        locationRepository: LocationRepository,
+    ) = CreateLocationUseCase(authService, locationRepository)
+
+    @ViewModelScoped
+    @Provides
+    fun provideCreateVehicleUseCase(
+        authService: AuthService,
+        vehicleRepository: VehicleRepository,
+    ) = CreateVehicleUseCase(authService, vehicleRepository)
 
 }
