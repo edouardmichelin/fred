7bb02a0515de0f391d6751c132cbf7b7d8dff0cd
edouardmichelin <edouard.michelin15@gmail.com>
2024-12-01 12:14:09 +0100

    feat: change username size in user profile

--- app/src/main/java/com/fred/app/presentation/profile/ProfileScreen.kt
+++ app/src/main/java/com/fred/app/presentation/profile/ProfileScreen.kt
@@ -5,38 +5,22 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.text.KeyboardOptions
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.*
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.style.TextOverflow
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
-import com.fred.app.R
 import com.fred.app.data.repository.model.Activity
-import com.fred.app.data.repository.model.Avatar
-import com.fred.app.data.repository.model.User
 import com.fred.app.presentation.home.IndeterminateCircularIndicator
-import com.fred.app.ui.component.DefaultScaffold
-import com.fred.app.ui.component.DefaultTextField
-import com.fred.app.ui.component.ToolbarWithEndIcon
-import kotlinx.coroutines.launch
 
 @SuppressLint("UnusedMaterialScaffoldPaddingParameter")
 @Composable
@@ -77,7 +61,7 @@ fun ProfileScreen(
         // Username and Score
         Text(
             text = user?.username!!,
-            style = MaterialTheme.typography.headlineSmall,
+            style = MaterialTheme.typography.headlineMedium,
             color = Color(0xFFB78700),
             modifier = Modifier.padding(bottom = 8.dp)
         )
